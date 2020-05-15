7775886699cdbe5a4da917f8126128c94d67090d
Martina Freundorfer <freundo@htw-berlin.de>
2020-05-15 11:15:22 +0200

    heart

---
+++ javascript/class.js
@@ -0,0 +1,29 @@
+let myLiteral = {
+    // ihr Object INhalt
+    // empfehlenswerte 
+};
+
+class Parent {
+    // Vererbung
+    constructor() {
+
+    }
+}
+
+// EcmaScript neue Variante für Konstruktoren 
+class MeinObjekt extends Parent {
+
+    constructor(x) {
+        super(); // Parent constructor wird damit gecallt 
+        this.something = x;
+    }
+
+}
+
+let myObject = new MeinObjekt(3);
+
+console.log(myObject.something);
+
+myObject.newProperty = 'oh';
+
+console.log(myObject.newProperty)
