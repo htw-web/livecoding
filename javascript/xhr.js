ef00bdb89739d8235dc84d271ca3f9e5595e67fb
Martina Freundorfer <freundo@htw-berlin.de>
2020-05-29 15:52:43 +0200

    up

--- javascript/xhr.js
+++ javascript/xhr.js
@@ -1,8 +1,9 @@
+// Oldschool 
 let xhr = new XMLHttpRequest();
 let url = "https://maps.googleapis.com/maps/api/geocode/json?" // Geocoding API 
-url = url + "address=Wilhelminenhofstr 75, Berlin"; 
+url = url + "address=Wilhelminenhofstr 75, Berlin";
 url = url + "&key=AIzaSyDxdHXv5NxBOOveXK8Uy-Nf6FeYUBwhvpY"; // ihren eigenen Key benutzen 
-xhr.open("GET", url, true);// methode, url, asynchron? 
+xhr.open("GET", url, true); // methode, url, asynchron? 
 xhr.onerror = function () { // diese Funktion wird ausgefuehrt, wenn ein Fehler auftritt
     alert("Connecting to server with " + url + " failed!\n");
 };
@@ -24,7 +25,7 @@ xhr.onload = function (e) { // diese Funktion wird ausgefuehrt, wenn die Anfrage
 };
 xhr.send();
 
-function requestHandler (response) { // diese Funktion wird ausgefuehrt, wenn die Anfrage erfolgreich war
+function requestHandler(response) { // diese Funktion wird ausgefuehrt, wenn die Anfrage erfolgreich war
     let data = response;
     let obj = JSON.parse(data); // auspacken
     console.log(obj);
@@ -41,8 +42,32 @@ function requestHandler (response) { // diese Funktion wird ausgefuehrt, wenn di
     }
 }
 
-/////////////////// Moderne Implementierung ///// 
+/////////////////// Moderne Implementierung Fetch API ///// 
 
 fetch(url)
-  .then(response => response.json()) // auspacken
-  .then(data => requestHandler(data));
+    .then(response => response.json()) // auspacken
+    .then(data => requestHandler(data))
+    .catch(console.log("Error handling"));
+
+
+/// event listen
+// moderner, css selector(class, id, tags), NodeElement kennen ihre Verwandtschaft => HTMLElement kennen nicht ihre Verwandtschaft
+let element = document.querySelector('#demo');
+element.addEventListener('submit', function (event) {
+    // verhindert das automatische neu laden der seite 
+    event.preventDefault();
+});
+
+let contacts = [{
+    firstName: "Martina",
+    lastName: "Freundorfer"
+}, {
+    firstName: "Martina",
+    lastName: "Freundorfer"
+}, {
+    firstName: "Martina",
+    lastName: "Freundorfer"
+}];
+
+contacts[0].plz = "14167";
+console.log(contacts);
