d4f623d901d1d90dd151725c2cee7b315233f30d
Martina Freundorfer <freundo@htw-berlin.de>
2020-05-29 12:11:48 +0200

    up

--- javascript/xhr.js
+++ javascript/xhr.js
@@ -7,13 +7,13 @@ xhr.onerror = function () { // diese Funktion wird ausgefuehrt, wenn ein Fehler 
     alert("Connecting to server with " + url + " failed!\n");
 };
 xhr.onload = function (e) { // diese Funktion wird ausgefuehrt, wenn die Anfrage erfolgreich war
-    var data = this.response;
-    var obj = JSON.parse(data);
+    let data = this.response;
+    let obj = JSON.parse(data);
     console.log(obj);
     if (this.status == 200) {
         if (obj.status != "ZERO_RESULTS") {
-            var lat = obj.results[0].geometry.location.lat;
-            var lng = obj.results[0].geometry.location.lng;
+            let lat = obj.results[0].geometry.location.lat;
+            let lng = obj.results[0].geometry.location.lng;
             console.log(lat + ", " + lng);
         } else {
             alert("Die Adresse konnte nicht aufgelöst werden!");
